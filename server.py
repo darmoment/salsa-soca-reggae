199d0be84e1dc7e4e77a100210898ffb682f8a31
Akinfolarin <akin.ogunmodede@gmail.com>
2024-12-11 14:01:07 -0500

    music tab complete

--- server.py
+++ server.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', data={}) 
 
+@app.route('/music')
+def music():
+    return render_template('music.html', data={}) 
 if __name__ == '__main__':
     app.run(debug=True)
